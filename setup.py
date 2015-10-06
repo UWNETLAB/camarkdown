93770ee8445440a1b235a9376733c8e7b8fac78b
Reid McIlroy-Young <reid@reidmcy.com>
2015-10-06 15:51:59 +0630

    Changed copy and paste error

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-setup(name='metaknowledge',
+setup(name='caMarkdown',
     version='0.0.1',
     author="Reid McIlroy-Young, John McLevey",
     author_email = "rmcilroy@uwaterloo.ca, john.mclevey@uwaterloo.ca",
