2d9314e5aeef3d5f58b08eca297cac7ed097fdca
Reid McIlroy-Young <reid@reidmcy.com>
2015-10-06 14:48:15 +0630

    Added gitignore and started parser

--- parser.py
+++ parser.py
@@ -1,2 +1,24 @@
+
+blockChar = '^'
+
 def tokenizer():
     pass
+
+def caParser(target, master):
+    targetIter = target.__iter__()
+    masterIter = master.__iter__()
+    while True:
+        try:
+            tChar = next(targetIter)
+        except StopIteration:
+            break
+        try:
+            mChar = next(masterIter)
+        except StopIteration:
+            raise Exception("Master is shorter than target.")
+        if tChar == mChar:
+            pass
+        else:
+            print("{} | {}".format(tChar, mChar))
+
+caParser("1234567890", "12345678kkkk")
