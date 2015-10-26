a4939ab6536621f1bcb3061916c1ba5cb1ebb7ca
Reid McIlroy-Young <reid@reidmcy.com>
2015-10-26 07:34:40 +0530

    Made _reload() work like you would expect
    Still not usable except for dev stuff

--- caMarkdown/__init__.py
+++ caMarkdown/__init__.py
@@ -5,11 +5,13 @@ import os.path
 
 def _reload():
     """This is for develop purposes only do not use otherwise.
-    _reload() is not to be trusted"""
+    _reload() is not to be trusted as it is evil and will create zombies"""
     import sys
     import importlib
-    sys.modules['caMarkdown'] = importlib.reload(sys.modules['caMarkdown'])
-    print("caMarkdown has been reloaded")
+    for modName in sys.modules.keys():
+        if "caMarkdown" == modName[:10]:
+            sys.modules[modName] = importlib.reload(sys.modules[modName])
+    print("caMarkdown has been reloaded, Zombies have risen")
 
 def test():
     loc = os.path.dirname(__file__) + "/tests"
