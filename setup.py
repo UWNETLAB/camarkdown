cc81f599eab8e3630ed4d6eabe3635e71175571f
Reid McIlroy-Young <reid@reidmcy.com>
2015-11-18 12:42:55 +0530

    Added dulwich to reqs

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(name='caMarkdown',
     version='0.0.1',
     author="Reid McIlroy-Young, John McLevey",
     author_email = "rmcilroy@uwaterloo.ca, john.mclevey@uwaterloo.ca",
+    install_requires= ['dulwich'],
     packages=['caMarkdown'],
     test_suite='caMarkdown.tests',
     entry_points={'console_scripts': [
